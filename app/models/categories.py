5b86d7947b38571e7f023c766687cb96b7aada6c
yemeal <emelyalex@bk.ru>
2026-03-28 23:07:29 +0700

    feat(db): create sqlalchemy model for categories

---
+++ app/models/categories.py
@@ -0,0 +1,37 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.base import Base
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .products import Product
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    name: Mapped[str] = mapped_column(
+        String(50),
+    )
+    is_active: Mapped[bool] = mapped_column(
+        default=True,
+    )
+    parent_id: Mapped[int | None] = mapped_column(
+        ForeignKey("categories.id"),
+    )
+
+    products: Mapped[list["Product"]] = relationship(
+        back_populates="categories",
+    )
+
+    # Параметр remote_side указывает ORM, какая колонка принадлежит удалённой стороне отношения,
+    # то есть стороне, на которую ссылается внешний ключ.
+    # В нашем случае remote_side="Category.id" означает что Category.id это колонка родительского объекта,
+    # а parent_id колонка дочернего.
+    parent: Mapped["Category | None"] = relationship(
+        back_populates="children",
+        remote_side="Category.id",
+    )
+    children: Mapped[list["Category"]] = relationship(
+        back_populates="parent",
+    )
