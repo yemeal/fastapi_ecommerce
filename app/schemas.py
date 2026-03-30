b879ecbd0f3a47fa84f845a6c8da6bb1a7d2cea6
yemeal <emelyalex@bk.ru>
2026-03-31 03:00:20 +0700

    feat(api): add pydantic schemas for User

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, EmailStr
 from decimal import Decimal
 
 
@@ -185,6 +185,68 @@ class Product(BaseModel):
         Field(
             ...,
             description="Активность товара",
+        ),
+    ]
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserCreate(BaseModel):
+    """
+    Модель для создания и обновления пользователя.
+    Используется в POST и PUT запросах.
+    """
+
+    email: Annotated[
+        EmailStr,
+        Field(
+            ...,
+            description="Email пользователя",
+        ),
+    ]
+    password: Annotated[
+        str,
+        Field(
+            ...,
+            min_length=8,
+            description="Пароль (минимум 8 символов)",
+        ),
+    ]
+    role: Annotated[
+        str,
+        Field(
+            default="buyer",
+            pattern="^(buyer|seller)$)",
+            description="Роль: 'buyer' или 'seller'",
+        ),
+    ]
+
+
+class User(BaseModel):
+    """
+    Модель для ответа с данными пользователя.
+    Используется в GET-запросах.
+    """
+
+    email: Annotated[
+        EmailStr,
+        Field(
+            ...,
+            description="Email пользователя",
+        ),
+    ]
+    is_active: Annotated[
+        bool,
+        Field(
+            ...,
+            description="Пользователь мягко удален или нет",
+        ),
+    ]
+    role: Annotated[
+        str,
+        Field(
+            ...,
+            description="Роль: 'buyer' или 'seller'",
         ),
     ]
 
