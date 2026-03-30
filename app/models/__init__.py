5fbaf6328284d83e07d804f565488a2892dfa5bc
yemeal <emelyalex@bk.ru>
2026-03-31 02:40:55 +0700

    feat(db): create User sqlalchemy model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,11 @@
 from .categories import Category
 from .products import Product
+from .users import User
 from .base import Base
 
 __all__ = [
     "Category",
     "Product",
     "Base",
+    "User",
 ]
---
+++ app/models/users.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy import String
+
+from app.models import Base
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .products import Product
+
+
+class User(Base):
+
+    email: Mapped[str] = mapped_column(
+        unique=True,
+        index=True,
+    )
+    hashed_password: Mapped[str] = mapped_column()
+    is_active: Mapped[bool] = mapped_column(
+        default=True,
+    )
+    role: Mapped[str] = mapped_column(
+        default="buyer",
+    )  # 'buyer' or 'seller'
+
+    products: Mapped[list["Product"]] = relationship(
+        back_populates="seller",
+    )
